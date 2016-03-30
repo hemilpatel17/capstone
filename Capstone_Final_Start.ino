d5a022059fc58e5ec3a61dcfc85304d89c11e2aa
hemilpatel17 <hemilpatel17@gmail.com>
2016-03-29 21:59:15 -0400

    Start of capstone project

---
+++ Capstone_Final_Start.ino
@@ -0,0 +1,105 @@
+//Import all the needed libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <UTFT.h>
+#include <UTouch.h>
+#include <UTFT_Buttons.h>
+#include <UTFT_SdRaw.h>
+//#include <SD.h>
+#include <SdFat.h>
+SdFat sd;
+
+//MP3 Player
+#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+#define DREQ            3      // VS1053 Data request, ideally an Interrupt pin
+
+//Card CS
+#define CARDCS 42     // Card chip select pin
+
+extern uint8_t SmallFont[];
+extern uint8_t BigFont[];
+
+//Create an instance of Mp3 Library
+Adafruit_VS1053_FilePlayer musicPlayer =
+Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+
+//Create an instance of UTFT + LCD screen
+UTFT myGLCD(EHOUSE70, 22,23,31,33);
+UTouch  myTouch(25,26,27,29,30);
+UTFT_SdRaw myFiles(&myGLCD);
+UTFT_Buttons  myButtons(&myGLCD, &myTouch);
+
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial) {
+        ;//need for due
+    }
+    
+    //TODO : Get the settings for volume and counter from due flash storge
+    
+    //Initialize the SD card
+    while(!sd.begin(CARDCS,SPI_FULL_SPEED)) {
+        Serial.println(F("Card failed to initialize, or not in the screen."));
+    }
+    Serial.println(F("Card Initialized successfully."));
+
+    //Initialize Music Pllayer (VS1053b)
+    while(!musicPlayer.begin()) {
+      Serial.println(F("Music player failed to initialized, continuously trying to initialize it"));
+      }
+     
+     musicPlayer.setVolume(20,20);
+     musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  
+    //Initialize LCD and UTFT
+    Serial.println(F("Initializing LCD & UTFT")); 
+    myGLCD.InitLCD();
+    myGLCD.clrScr();
+    myGLCD.setFont(SmallFont);
+    myTouch.InitTouch();
+    myTouch.setPrecision(PREC_EXTREME);
+    myButtons.setTextFont(BigFont);
+ 
+    //TODO : Add 'Hello I am pouchy come talk to me'
+    //TODO : Add a picture of hello
+    //TODO : Add a button that will say 'Settings'
+    //TODO : if settting then go to admin menu 
+    
+
+    startUp();
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    
+}
+
+void startUp() {
+    int adminPress = 0;
+    int x = 0;
+    int y = 0;
+    myButtons.deleteAllButtons();
+    myGLCD.clrScr();
+    myGLCD.fillScr(255,255,255);
+  
+      //Start up screen and audio out for start up message
+    myFiles.load(0,0,800,480,"PICTURES/hello.raw",1,1);
+    int admin =  myButtons.addButton(599,0,200,50,"ADMIN");
+    myButtons.drawButton(admin);
+   
+    while(1){
+      myTouch.read();
+      x=myTouch.getX();
+      y=myTouch.getY();
+      if((x >= 10 && x <= 30) && (y >= 180 && y <= 240)) {
+        Serial.println("Touch detected");
+        } 
+    }
+    Serial.println(F("Playing 'Hello Message.'"));
+    musicPlayer.playFullFile("MESSAGES/hello.mp3");
+  
+  }
